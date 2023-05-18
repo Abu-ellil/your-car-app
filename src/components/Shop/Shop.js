935109dfafdf1d09a26b351893cd30213f73b734
Abo Ellil <mr.abuellil@gmail.com>
2023-05-18 17:36:52 +0300

    0

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCartPlus,
@@ -69,6 +69,27 @@ const Shop = () => {
     }
   }, []);
 
+// ===============================================
+
+const cardRef = useRef();
+const [width, setWidth] = useState(0);
+const [isMobileView, setIsMobileView] = useState(false);
+
+useEffect(() => {
+  setWidth(cardRef.current.scrollWidth - cardRef.current.offsetWidth);
+  const handleResize = () => {
+    setIsMobileView(window.innerWidth < 768);
+  };
+  handleResize();
+  window.addEventListener("resize", handleResize);
+  return () => {
+    window.removeEventListener("resize", handleResize);
+  };
+}, []);
+
+const carouselItemsToShow = isMobileView ? 1 : 3;
+
+// ====================================================
   return (
     <div id="cars" className="main-shop">
       <Nav toggel={handleCartToggle} count={cartCount} />
@@ -78,51 +99,53 @@ const Shop = () => {
           <h2>Cars</h2>
         </div>
         <div className="car-container">
-          <Carousel itemsToShow={3}>
-            {carsData.map((car, index) => (
-              <div className="car-card" key={index}>
-                <img src={car.image} alt={car.name} className="car-image" />
-                <div className="car-details">
-                  <h3>{car.name}</h3>
-                  <p>{car.class}</p>
-                  <p>{car.description}</p>
-                </div>
-                <div className="car-card-bottom">
-                  <div className="info">
-                    <div>
-                      <p>
-                        <FontAwesomeIcon icon={faUsers} /> {car.seats} Seats
-                      </p>
+          <div ref={cardRef} className="car-container">
+            <Carousel itemsToShow={carouselItemsToShow}>
+              {carsData.map((car, index) => (
+                <div className="car-card" key={index}>
+                  <img src={car.image} alt={car.name} className="car-image" />
+                  <div className="car-details">
+                    <h3>{car.name}</h3>
+                    <h4>{car.class}</h4>
+                    <p>{car.description}</p>
+                  </div>
+                  <div className="car-card-bottom">
+                    <div className="info">
+                      <div>
+                        <p>
+                          <FontAwesomeIcon icon={faUsers} /> {car.seats} Seats
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          <FontAwesomeIcon icon={faSuitcase} /> {car.luggage}{" "}
+                          Luggage
+                        </p>
+                      </div>
                     </div>
-                    <div>
-                      <p>
-                        <FontAwesomeIcon icon={faSuitcase} /> {car.luggage}{" "}
-                        Luggage
-                      </p>
+                    <div className="counter">
+                      <button
+                        className="counter-btn minus"
+                        onClick={() => handleRemoveFromCart(index)}
+                        disabled={!cartItems.includes(car)}
+                      >
+                        <FontAwesomeIcon icon={faMinus} />
+                      </button>
+                      <span>
+                        {cartItems.filter((item) => item === car).length}
+                      </span>
+                      <button
+                        className="counter-btn plus"
+                        onClick={() => handleAddToCart(car)}
+                      >
+                        <FontAwesomeIcon icon={faCartPlus} />
+                      </button>
                     </div>
                   </div>
-                  <div className="counter">
-                    <button
-                      className="counter-btn minus"
-                      onClick={() => handleRemoveFromCart(index)}
-                      disabled={!cartItems.includes(car)}
-                    >
-                      <FontAwesomeIcon icon={faMinus} />
-                    </button>
-                    <span>
-                      {cartItems.filter((item) => item === car).length}
-                    </span>
-                    <button
-                      className="counter-btn plus"
-                      onClick={() => handleAddToCart(car)}
-                    >
-                      <FontAwesomeIcon icon={faCartPlus} />
-                    </button>
-                  </div>
                 </div>
-              </div>
-            ))}
-          </Carousel>
+              ))}
+            </Carousel>
+          </div>
         </div>
       </div>
       {/* ====================CART========================== */}
